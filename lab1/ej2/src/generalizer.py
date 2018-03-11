619b46903ec8f5bf97f2a26cfb4dc28adb1acc4d
Toni Cabrera <acabreragnz@gmail.com>
2018-03-11 18:12:33 -0300

    fix: el generalizer tenia algunos detalles por mal entendimiento

--- lab1/ej2/src/generalizer.py
+++ lab1/ej2/src/generalizer.py
@@ -1,28 +1,28 @@
-def gen(training_examples, v_params, moderate_constant=0.1):
+def gen(training_examples, initial_weights, moderate_constant=0.1):
     """
         :param training_examples: array con tupla con la forma (board_features, v_op_applied_to_board)
-        :param v_params: tupla con la forma (weights, board_features)
+        :param initial_weights:
         :param moderate_constant: constante de ajuste del algoritmo LMS
 
         :return: los nuevos parametros de la funcion v, con la forma (weights, board_features)
     """
 
-    board_features = v_params[1]
-    calculated_weights = v_params[0]
+    calculated_weights = initial_weights
 
     for training_example in training_examples:
         current_board_features = training_example[0]
         v_train_applied_to_board = training_example[1]
 
-        new_v_params = (calculated_weights, current_board_features)
-        v_op_applied_to_board = apply_v(new_v_params)
+        # calculo V utilizando el board de entrenamiento actual y los pesos que voy calculando
+        v_op_applied_to_board = apply_v((calculated_weights, current_board_features))
+
+        # se calculan los nuevos pesos
 
         for index, wi in calculated_weights:
             error = v_train_applied_to_board - v_op_applied_to_board
+            calculated_weights[index] = wi + moderate_constant * error * current_board_features[index]
 
-            calculated_weights[index] = wi + moderate_constant * error * board_features[index]
-
-    return calculated_weights, board_features
+    return calculated_weights
 
 
 def apply_v(v_params):
