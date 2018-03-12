3c995b7748c5d2de8ae7c4a65d7fc12b2129afd3
Toni Cabrera <acabreragnz@gmail.com>
2018-03-11 21:10:41 -0300

    fix: missing enumerate

--- lab1/ej2/src/tablero.py
+++ lab1/ej2/src/tablero.py
@@ -11,7 +11,7 @@ def apply_v(v_params):
     base_weight = weights[0]
     sum_weight_features = 0
 
-    for index, board_feature in board_features:
+    for index, board_feature in enumerate(board_features):
         sum_weight_features += weights[index + 1] * board_features[index]
 
     return base_weight + sum_weight_features
