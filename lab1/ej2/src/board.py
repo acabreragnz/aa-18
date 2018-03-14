0f1903a5a942cb3cb00f3422bd80709605faae69
Bruno Piaggio <brunopiaggiok@gmail.com>
2018-03-14 15:19:08 -0300

    Cambios en random_movement(board_matrix, turn) y move(board,turn,W,n)
    Se modifica random_movement(board_matrix, turn) para que la representacion de las fichas sea consistente con el resto del codigo (enteros 1 o 2)
    
    Se saca el parametro n de move(board,turn,W,n), se importa desde el modulo lab1/ej2/src/constants.py

--- lab1/ej2/src/board.py
+++ lab1/ej2/src/board.py
@@ -2,30 +2,27 @@ from copy import deepcopy
 from random import randint
 from lab1.ej2.src.converter import convert
 from lab1.ej2.src.utils import apply_v
+from lab1.ej2.src.constants import N
 
-import lab1.ej2.src.constants as const
 
-
-def random_movement(board_matrix, turn):
+def random_movement(board, turn):
     """
     Realiza un movimiento aleatorio para el jugador cuyo turno se indique en turn
-    :param board_matrix: Matriz cuadrada representando el estado del juego
-    :param turn: BLACK_TURN o WHITE_TURN
+
+    :param board: Matriz cuadrada representando el estado del juego (board[i][j] in [0, 1, 2] para todo (i,j))
+    :param turn: Turno que le corresponde al jugador que quiere realizar el movimiento aleatorio (turn in [1,2])
     :return: Una nueva matriz con el estado del juego luego del movimiento
     """
-    new_board_matrix = deepcopy(board_matrix)
-    f = const.BLACK
-    if turn == const.WHITE_TURN:
-        f = const.WHITE
+    new_board = deepcopy(board)
     while True:
-        i = randint(0, const.N - 1)
-        j = randint(0, const.N - 1)
-        if new_board_matrix[i][j] == const.VOID:
-            new_board_matrix[i][j] = f
-            return new_board_matrix
+        i = randint(0, N - 1)
+        j = randint(0, N - 1)
+        if new_board[i][j] == 0:
+            new_board[i][j] = turn
+            return new_board
 
 
-def move(board,turn,W,n):
+def move(board, turn, W):
     #V es una funcion de evaluacion que asigna una puntuacion numerica a cualquier estado de tablero.
     #Pretendemos que esta funcion objetivo V asigne puntuaciones mas altas a mejores estados de tablero.
     #Obener la mejor jugada se puede lograr generando el estado del tablero sucesor producido por cada jugada legal, 
@@ -34,17 +31,17 @@ def move(board,turn,W,n):
     #Determinar todas las jugadas posibles para el turno pasado por parametro - para cada celda vacia de la matriz purebo a colocar una ficha del color
     #del turno que esta jugando
     
-    v_max = apply_v( (W, convert(board)) );    
-    board_next = [[board[x][y] for x in range(n)] for y in range(n)] 
+    v_max = apply_v((W, convert(board)))
+    board_next = [[board[x][y] for x in range(N)] for y in range(N)]
     board_result = []
-    for i in range (0, n):
-        for j in range (0, n):
-            if board_next[i][j] == 0 :
+    for i in range(0, N):
+        for j in range(0, N):
+            if board_next[i][j] == 0:
                 board_next[i][j] = turn
-                v_result = apply_v( (W,convert(board_next)) )
-                if v_result >= v_max :
+                v_result = apply_v((W, convert(board_next)))
+                if v_result >= v_max:
                     v_max = v_result
-                    board_result = [[board_next[x][y] for x in range(n)] for y in range(n)] 
+                    board_result = [[board_next[x][y] for x in range(N)] for y in range(N)]
                 board_next[i][j] = 0
     
     return board_result    
