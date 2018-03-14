1fe51798c2e3755d79aeea79760ec1e6d4198b7b
Bruno Piaggio <brunopiaggiok@gmail.com>
2018-03-14 15:32:02 -0300

    3 nuevas funciones en lab1/ej2/performance_system
    - get_game_trace_with_random_player (implementada)
    - get_game_trace_with_human_player
    - get_game_trace_with_previous_version

---
+++ lab1/ej2/src/performance_system.py
@@ -0,0 +1,51 @@
+from lab1.ej2.src.board import move, random_movement
+from lab1.ej2.src.converter import convert
+from lab1.ej2.src.utils import isgameover
+
+
+def get_game_trace_with_random_player(initial_board, weights):
+    """
+    Genera una traza de un juego maquina vs maquina hasta el final en donde la ultima realiza movimientos aleatorios.
+
+    :param initial_board: Matriz con el movimiento del jugador 1 realizado
+    :param weights: Tupla con los pesos de la funcion v_op que se tienen actualmente
+    :return: Lista de tuplas representando el estado del juego luego de cada movimiento de cada jugador
+    """
+
+    # El movimiento del juagdor 1 ya fue hecho en initial_board
+    game_trace = [convert(initial_board)]
+    board = initial_board
+    while True:
+        # Turno del jugador 2 (jugador aleatorio)
+        board = random_movement(board, 2)
+        game_trace.append(convert(board))
+        if isgameover(game_trace[-1]):
+            break
+        else:
+            # Turno del jugador 1 (jugador que busca la funcion v)
+            board = move(board, 1, weights)
+            game_trace.append(convert(board))
+            if isgameover(game_trace[-1]):
+                break
+    return game_trace
+
+
+def get_game_trace_with_human_player():
+    """
+    Genera una traza de un juego maquina vs humano hasta el final.
+    Obtiene los movimientos de la persona desde la entrada estandar.
+
+    :return: Lista de tuplas representando el estado del juego luego de cada movimiento de cada jugador
+    """
+
+    pass
+
+
+def get_game_trace_with_previous_version():
+    """
+    Genera una traza de un juego maquina vs maquina hasta el final en donde la ultima obtiene los movimientos
+    utilizando otra funcion de valoracion v_op_prev. Obtiene los pesos de v_op_prev de la entrada estandar.
+
+    :return: Lista de tuplas representando el estado del juego luego de cada movimiento de cada jugador
+    """
+    pass
