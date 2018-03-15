2411c9dc572778e71825df97b40926e7c7184a52
Bruno Piaggio <brunopiaggiok@gmail.com>
2018-03-15 17:54:08 -0300

    Fix funcion gen
    Cambio la forma de calcular v_op_applied_to_board y comento la normalizacion

--- lab1/ej2/src/generalizer.py
+++ lab1/ej2/src/generalizer.py
@@ -17,12 +17,11 @@ def gen(training_examples, initial_weights, moderate_constant=0.1):
         v_train_applied_to_board = training_example[1]
 
         # calculo V utilizando el board de entrenamiento actual y los pesos que voy calculando
-        #v_op_applied_to_board = apply_v((calculated_weights, current_board_features))
-        v_op_applied_to_board = apply_v((initial_weights, current_board_features))
+        v_op_applied_to_board = apply_v((calculated_weights, current_board_features))
 
-        norm = 1
-        for board_feature in current_board_features:
-            norm = norm + board_feature
+        #norm = 1
+        #for board_feature in current_board_features:
+        #    norm = norm + board_feature
 
         error = v_train_applied_to_board - v_op_applied_to_board
         print("LMS error is", error)
@@ -32,7 +31,7 @@ def gen(training_examples, initial_weights, moderate_constant=0.1):
             if index == 0:
                 calculated_weights[index] = wi
             else:
-                calculated_weights[index] = wi + moderate_constant * error * current_board_features[index-1]/norm
+                calculated_weights[index] = wi + moderate_constant * error * current_board_features[index-1]#/norm
 
         #print(calculated_weights)
     return calculated_weights
