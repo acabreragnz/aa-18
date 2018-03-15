5880651401bc99733824ca6dfefaf5e56ee8373f
Bruno Piaggio <brunopiaggiok@gmail.com>
2018-03-15 13:37:42 -0300

    Fix funcion get_game_trace_with_random_player

--- lab1/ej2/src/performance_system.py
+++ lab1/ej2/src/performance_system.py
@@ -6,24 +6,22 @@ def get_game_trace_with_random_player(initial_board, weights):
     """
     Genera una traza de un juego maquina vs maquina hasta el final en donde la ultima realiza movimientos aleatorios.
 
-    :param initial_board: Matriz con el movimiento del jugador 1 realizado
+    :param initial_board: Instancia de Board
     :param weights: Tupla con los pesos de la funcion v_op que se tienen actualmente
     :return: Lista de tuplas representando el estado del juego luego de cada movimiento de cada jugador
     """
 
     # El movimiento del juagdor 1 ya fue hecho en initial_board
-    game_trace = [convert(initial_board)]
     board = initial_board
+    game_trace = [board.to_features()]
     while True:
         # Turno del jugador 2 (jugador aleatorio)
-        board.random_movement(board, 2)
-        game_trace.append(convert(board))
+        board.random_movement(Board.WHITE_PIECE, game_trace)
         if Board.is_game_over(game_trace[-1]):
             break
         else:
             # Turno del jugador 1 (jugador que busca la funcion v)
-            board = board.best_move(board, 1, weights)
-            game_trace.append(convert(board))
+            board.best_move(board, 1, weights)
             if Board.is_game_over(game_trace[-1]):
                 break
     return game_trace
