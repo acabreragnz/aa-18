271e829bb1dca9eaed4d40bd088d9319222dc1b1
Bruno Piaggio <brunopiaggiok@gmail.com>
2018-03-15 22:00:22 -0300

    Agrego test_squared_errors_4

---
+++ lab1/ej2/src/test_performance_system.py
@@ -0,0 +1,13 @@
+import unittest
+from board import Board
+from performance_system import get_game_trace_with_random_player
+from experiment_generator import experiment_generator
+
+
+class TestPerformanceSystem(unittest.TestCase):
+
+    def test_get_game_trace_with_random_player_finished_game(self):
+        weights = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        board = experiment_generator()
+        game_trace = get_game_trace_with_random_player(board, weights)
+        self.assertTrue(Board.is_game_over_from_features(game_trace[-1]), 'El juego debe estar terminado')
