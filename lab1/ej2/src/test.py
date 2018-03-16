5b5fcac729d0f36e73c94d583f8734025f922585
Constanza Dieci <cdieci@perceptivatech.com>
2018-03-16 15:10:21 -0300

    cambio len -2 por len -1 Agrego los casos en que pierde, gana o empata

--- lab1/ej2/src/test.py
+++ lab1/ej2/src/test.py
@@ -1,6 +1,6 @@
 from experiment_generator import experiment_generator
 from board import Board
-from critic import get_training_examples, get_initial_training_examples
+from critic import get_training_examples
 import generalizer
 from constants import N
 
@@ -8,23 +8,18 @@ from constants import N
 n = N
 
 # Invento unos pesos para probar
-weights = [0,1,2,2,3,3,4,4,1,2,2,3,3,4,4]
-#weights = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-
+#weights = [0,1,2,2,3,3,4,4,1,2,2,3,3,4,4]
+weights = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+weights_old = weights
 
 def print_training(training_examples):
     for i in range(len(training_examples)):
         print(training_examples[i])
 
 
-training_examples = get_initial_training_examples()
-#print_training(training_examples)
-weights = generalizer.gen(training_examples, weights, 0.3)
-print("########### Weights 1 ###########")
-print(weights)
-
 game_trace = []
-for i in range(10):
+iter = 6
+for i in range(iter):
 
     board = experiment_generator()
     # T es un tablero con la primer ficha negra colocada en una posicion aleatoria
@@ -37,17 +32,14 @@ for i in range(10):
             board.best_move(turn, weights, game_trace)
             turn = Board.WHITE_PIECE
         elif turn == board.WHITE_PIECE:
-            board.random_movement(turn, game_trace)
+            board.best_move(turn, weights_old, game_trace)
             turn = Board.BLACK_PIECE
 
 
-
-training_examples = get_training_examples(game_trace, weights)
-weights1 = generalizer.gen(training_examples, weights, 0.3)
-print("########### Weights 2 ###########")
-print(weights1)
-
-training_examples = get_training_examples(game_trace, weights)
-weights2 = generalizer.gen(training_examples, weights, 0.3)
-print("########### Weights 3 ###########")
-print(weights2)
+    u = (iter - i) * 0.1
+    print(u)
+    weights_old = weights
+    training_examples = get_training_examples(game_trace, weights)
+    weights = generalizer.gen(training_examples, weights, u)
+    print(weights)
+    print_training(training_examples)
