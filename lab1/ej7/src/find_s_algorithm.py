30b1b28373e77234d5cf5edf7bddbe8fc4f5496b
Toni Cabrera <acabreragnz@gmail.com>
2018-03-17 22:46:58 -0300

    fix calculo de promedios

--- lab1/ej7/src/find_s_algorithm.py
+++ lab1/ej7/src/find_s_algorithm.py
@@ -1,32 +1,60 @@
-#FIND-SAlgorithm.
+# FIND-SAlgorithm.
 
-#1. Initialize h to the most specific hypothesis in H 
-#2. Foreachpositivetraininginstancex
-	# For each attribute constraint a, in h
-		#If the constraint a, is satisfied by x
-			#Then do nothing
-		#Else replace a, in h by the next more general constraint that is satisfied by x
-#3. Outputhypothesish
+# 1. Initialize h to the most specific hypothesis in H
+# 2. Foreachpositivetraininginstancex
+# For each attribute constraint a, in h
+# If the constraint a, is satisfied by x
+# Then do nothing
+# Else replace a, in h by the next more general constraint that is satisfied by x
+# 3. Outputhypothesish
 
 from constants import NO_VALUE, ANY_VALUE
 
 
 def find_s(training_examples):
-	h = [NO_VALUE,  NO_VALUE,  NO_VALUE, NO_VALUE, NO_VALUE]
-	for example in training_examples:
-		if example[1] == 1:
-			tupla = example[0]
-			for i in range(0,5):
-				if h[i] == NO_VALUE:
-					h[i] = tupla[i]
-				elif h[i] != tupla[i]:
-					h[i] = ANY_VALUE
-				
-				
-	return h				
+    h = [NO_VALUE, NO_VALUE, NO_VALUE, NO_VALUE, NO_VALUE]
+    for example in training_examples:
+        if example[1] == 1:
+            tupla = example[0]
+            for i in range(0, 5):
+                if h[i] == NO_VALUE:
+                    h[i] = tupla[i]
+                elif h[i] != tupla[i]:
+                    h[i] = ANY_VALUE
+
+    return h
 
 
+def find_s_target(training_examples, target):
+    total_evaluados = 0
+    hay_match = False
+    positivos_evaluados = 0
+    h = [NO_VALUE, NO_VALUE, NO_VALUE, NO_VALUE, NO_VALUE]
+    for index, example in enumerate(training_examples):
+        if example[1] == 1:
+            positivos_evaluados += 1
+            tupla = example[0]
+            for i in range(0, 5):
+                if h[i] == NO_VALUE:
+                    h[i] = tupla[i]
+                elif h[i] != tupla[i]:
+                    h[i] = ANY_VALUE
+
+            if match_target(h, target):
+                hay_match = True
+                total_evaluados = index + 1
+                break
+
+    return h, hay_match, total_evaluados, positivos_evaluados
 
 
+def match_target(h, target):
+    match = True
+    largo_h = len(h)
+    index = 0
 
+    while index < largo_h and match:
+        match = h[index] == target[index]
+        index += 1
 
+    return match
--- lab1/ej7/src/main.py
+++ lab1/ej7/src/main.py
@@ -1,7 +1,9 @@
-from find_s_algorithm import find_s
+from find_s_algorithm import find_s, find_s_target
 from training_examples import example1, random_training_examples
 import logging
+from constants import ANY_VALUE, Dificultad
 from random import randint
+import numpy as np
 
 logging.basicConfig(filename='find_s_algorithm.log', level=logging.INFO)
 
@@ -20,12 +22,23 @@ logging.info('Started')
 
 
 logging.info(f"#Ejemplos;    #Ejemplos+;      c = <?, Media, ?, ?, ?>")
-for n in range(1,101):
+
+lista_positivos_evaluados = []
+lista_total_evaluados = []
+for n in range(1, 101):
 
     for i in range(10):
         cant_pos = randint(1,n)
         ex2 = random_training_examples(n, cant_pos);
-        h = find_s(ex2)
-        logging.info(f"{n};          {cant_pos};     {h}")
+        (h, hay_match, total_evaluados, positivos_evaluados) = find_s_target(ex2, [ANY_VALUE, Dificultad.MEDIA, ANY_VALUE, ANY_VALUE, ANY_VALUE])
 
+        if hay_match:
+            lista_total_evaluados.append(total_evaluados)
+            lista_positivos_evaluados.append(positivos_evaluados)
+
+            logging.info(f"n={n}; cant_positivos_total={cant_pos}; h={h}  total_evaluados={total_evaluados} positivos_evaluados={positivos_evaluados}")
+        else:
+            logging.info(f"n={n}; cant_positivos_total={cant_pos}; h={h}")
+
+logging.info(f"promedio_total={np.average(lista_total_evaluados)}; promedio_positivos={np.average(lista_positivos_evaluados)}")
 
