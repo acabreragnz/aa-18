44bd1de78145018683e891a5f8ed4c967e3bf645
Bruno Piaggio <brunopiaggiok@gmail.com>
2018-03-20 00:02:21 -0300

    Fix metodo get_training_examples

--- lab1/ej2/src/critic.py
+++ lab1/ej2/src/critic.py
@@ -11,8 +11,8 @@ def get_training_examples(game_trace, weights):
     """
 
     training_examples = []
-    i = 0
-    while i < game_trace.__len__() - 2:
+    n = game_trace.__len__()
+    for i in range(1, n-2, 2):
         board_features = game_trace[i]
 
         # game_trace[i + 2] representa el estado de juego luego de la respuesta del oponente al estado game_trace[i]
@@ -21,14 +21,15 @@ def get_training_examples(game_trace, weights):
         v_ent_value_for_current_board_features = apply_v(my_next_turn_board_features, weights, Board.BLACK_PIECE)
 
         training_examples.append((board_features, v_ent_value_for_current_board_features))
-        i += 2
 
     # Se agrega el ultimo estado con un valor fijo (solo si hubo ganador)
+    if n % 2:
+        if Board.won_black_from_features(game_trace[-1]):
+            training_examples.append((game_trace[-1], 1))
+    else:
+        if Board.won_white_from_features(game_trace[-1]):
+            training_examples.append((game_trace[-2], -1))
 
-    if Board.won_black_from_features(game_trace[-1]):
-        training_examples.append((game_trace[-1], 1))
-    elif Board.won_white_from_features(game_trace[-1]):
-        training_examples.append((game_trace[-1], -1))
 
     return training_examples
 
