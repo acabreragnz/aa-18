d7bc2f386a5351df1448c702ecacba94b3e54b1d
Toni Cabrera <acabreragnz@gmail.com>
2018-03-27 15:09:44 -0300

    agrega estructura para ej 5

---
+++ lab2/ej5/src/decision_tree.py
@@ -0,0 +1,18 @@
+class DecisionTree:
+    def __init__(self):
+        self.tree = None # La implementacion que se use para arbol
+
+    def id3(self, instances):
+        return None
+
+    def all_instances_have_same_value(self, instances):
+        return None
+
+    def choose_best_attribute(self):
+        return None
+
+    def calculate_gain(self, atribute, instances):
+        return None
+
+    def calculate_entropy(self, attribute):
+        return None
